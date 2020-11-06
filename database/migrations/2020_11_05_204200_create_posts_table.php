5d46010eab137af44d543e320762e687de267d3e
Ramon Ramirez <eduardoramirez0104@gmail.com>
2020-11-06 02:12:35 -0400

    Añadi las columnas, llaves primarias y foraneas a post y categorias

--- database/migrations/2020_11_05_204200_create_posts_table.php
+++ database/migrations/2020_11_05_204200_create_posts_table.php
@@ -14,7 +14,10 @@ class CreatePostsTable extends Migration
     public function up()
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('body');
+            $table->unsignedBigInteger('category_id');
             $table->timestamps();
         });
     }
