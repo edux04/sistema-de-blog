36db570055b1c20877601eaf5cb6f646ba65002b
Ramon Ramirez <eduardoramirez0104@gmail.com>
2020-11-08 04:32:02 -0400

    rutas para la api ver todos y solo un articulo

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/posts', "App\Http\Controllers\Api\PostController@index");
+Route::get('/posts/{articulo}', "App\Http\Controllers\Api\PostController@show");
