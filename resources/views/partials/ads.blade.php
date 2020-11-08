3ebc9196a69058b470aa9dc380276dc6877d0fae
Ramon Ramirez <eduardoramirez0104@gmail.com>
2020-11-08 18:54:18 -0400

    Se añade logos y publicidad simulada, se corrige el tipo de datos del campo body en los articulos

---
+++ resources/views/partials/ads.blade.php
@@ -0,0 +1,11 @@
+<!-- Latest Post -->
+<div class="widget widget-latest-post">
+    <div class="widget-title">
+        <h3>Te puede interesar</h3>
+    </div>
+
+    <img src="{{ URL::to('/') }}/images/publicidad/publicidad.webp" alt="quesos michel" class="publicidad">
+
+    <img src="{{ URL::to('/') }}/images/publicidad/publicidad2.webp" alt="funeraria" class="publicidad">
+
+</div>
---
+++ resources/views/posts/search.blade.php
@@ -0,0 +1,57 @@
+@extends('partials.template')
+
+
+
+
+@section('title')
+    Resultado{{ count($articulos) > 1 ? 's' : '' }} de la busqueda
+@endsection
+
+
+@section('content')
+
+    <div class="row front-page gray-bg">
+        <div class=" col-12">
+            <h3 class="big-article-title">Resultado{{ count($articulos) > 1 ? 's' : '' }} de la busqueda</h3>
+        </div>
+        <!-- Blog articles Column -->
+        <div class="col-md-8  ">
+
+            @foreach ($articulos as $articulo)
+                <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+                    <div class="big-article">
+                        <a href="{{ $articulo->publicUrl() }}">
+
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="image">
+                                        @if ($articulo->image)
+                                            <img src="{{ asset('storage/' . $articulo->image) }}" alt="titulo de la imagen">
+                                        @endif
+                                    </div>
+                                </div>
+                                <div class="col-sm-12">
+                                    <div class="blog-details">
+                                        <h2 class="big-article-title">{{ $articulo->title }}</h2>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+
+
+
+        <!-- Sidebar Widgets Column -->
+        <div class="col-lg-4 m-15px-tb blog-aside">
+
+            @include('partials.ads')
+        </div>
+
+    </div>
+
+@endsection
